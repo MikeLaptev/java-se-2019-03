234e4142bfc71ff07a177ac485b9784f450dc399
Mikhail Laptev <mikhail.laptev@gmail.com>
2019-04-13 16:51:05 +0100

    Adding 'Hello World' using Guava for Otus

---
+++ hw01-maven/src/main/java/org/mlaptev/otus/HelloOtus.java
@@ -0,0 +1,27 @@
+package org.mlaptev.otus;
+
+import com.google.common.graph.GraphBuilder;
+import com.google.common.graph.Graphs;
+import com.google.common.graph.MutableGraph;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class HelloOtus {
+
+  private static Logger logger = LogManager.getLogger(HelloOtus.class);
+
+  public static void main(String[] args) {
+
+    MutableGraph<String> graph =
+        GraphBuilder.undirected()
+            .allowsSelfLoops(false)
+            .build();
+    graph.putEdge("a", "b");
+    graph.putEdge("b", "c");
+    graph.putEdge("c", "a");
+
+    if (Graphs.hasCycle(graph)) {
+      logger.error("Graph {} has cycle(s)!", graph);
+    }
+  }
+}
