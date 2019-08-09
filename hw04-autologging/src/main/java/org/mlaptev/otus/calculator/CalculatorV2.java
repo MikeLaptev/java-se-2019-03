ffa0401a0a377d757eb9b366ccb033b4592a7635
Mikhail Laptev <mikhail.laptev@gmail.com>
2019-08-09 22:45:16 +0100

    Adding @Overrides

--- hw04-autologging/src/main/java/org/mlaptev/otus/calculator/CalculatorV2.java
+++ hw04-autologging/src/main/java/org/mlaptev/otus/calculator/CalculatorV2.java
@@ -2,14 +2,17 @@ package org.mlaptev.otus.calculator;
 
 public class CalculatorV2 implements CalculatorInterface {
 
+  @Override
   public int add(int a, int b) {
     return -(a + b);
   }
 
+  @Override
   public int subtract(int a, int b) {
     return -(a - b);
   }
 
+  @Override
   public int multiply(int a, int b) {
     return -(a * b);
   }
